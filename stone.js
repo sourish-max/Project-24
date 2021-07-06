b39854d37904b8c608dc36b72724c036f7e4e91f
sourish-max <76769790+sourish-max@users.noreply.github.com>
2021-07-06 15:39:23 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,10 @@
+class Stone{
+    constructor(x,y,width,height){
+        var option={
+            'restitution':0.8,
+            'density':12,
+            'friction':0.9
+        }
+        this.body=Bodies.rectangle(x, y, width, height , options);
+    }
+}
